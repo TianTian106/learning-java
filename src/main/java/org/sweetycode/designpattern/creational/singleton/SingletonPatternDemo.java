fd94916ae63c19934cf68bf94b08482dd92b9e3f
田甜 <tiantianchn@163.com>
2019-04-13 19:34:06 +0800

    add multithreading test

--- src/main/java/org/sweetycode/designpattern/creational/singleton/SingletonPatternDemo.java
+++ src/main/java/org/sweetycode/designpattern/creational/singleton/SingletonPatternDemo.java
@@ -1,10 +1,36 @@
 package org.sweetycode.designpattern.creational.singleton;
 
+import java.util.Objects;
+import java.util.concurrent.*;
+
 public class SingletonPatternDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         EagerSingleObject object = EagerSingleObject.getInstance();
         object.showMessage();
         EnumSingleObject.INSTANCE.sayID();
         System.out.println(EnumSingleObject.INSTANCE == EnumSingleObject.INSTANCE);
+
+
+        //测试验证50个线程获取单例是一个实例
+
+        ExecutorService executorService = Executors.newFixedThreadPool(50);
+        Future<Integer>[] future = new Future[50];
+        for (int i = 0; i < 50; i ++) {
+            future[i] = executorService.submit(() -> SISingleObject.getInstance().hashCode());
+        }
+
+        try {
+            // Integer code = (int) future[0].get();
+            Integer code = future[0].get();
+            for (Future i: future) {
+                if (!Objects.equals(code, i.get()))
+                    //if (code != (int) i.get())
+                    throw new Exception("It's not single!");
+            }
+        } finally {
+            executorService.shutdown();
+        }
+
+
     }
 }
